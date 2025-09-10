58ec63b1ab422c5b9a6604ff2aa63289a4086bbf
HEGATNB <misharybakov2006@gmail.com>
2025-09-10 14:00:04 +0300

    Рабочий вариант

--- calculator.cs
+++ calculator.cs
@@ -1,17 +1,86 @@
-int a = 0;
-int b = 0;
-console.writeline("Проект 'калькулятор' на C#");
-console.writeline("Для выхода введите exit, для использования калькулятора введите 1 число, 2 число и операцию.");
-while (true){
-    var input = Console.ReadLine(); 
-    if (input = "exit"){
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+double memory = 0;
+double val = 0;
+bool firstInput = true;
+char[] TwoNumOperators = { '+', '-', '/', '*', '%', '^' };
+string[] OneNumOperators = { "M+", "M-", "MR" };
+
+Console.WriteLine("Проект 'калькулятор' на C#");
+Console.WriteLine("Для выхода введите exit \nДля использования калькулятора введите 1 число");
+Console.WriteLine("Список операций: + - / * % ^ M+ M- MR");
+while (true)
+{
+    string input = Console.ReadLine().Trim();
+
+    if (input == "exit")
+    {
         break;
     }
-    foreach (char c in input){
-        if ( Char.IsDigit()){
-               if (convert.ToInt(input)){
-                break;
-               }
+
+    if (firstInput)
+    {
+        val = Convert.ToDouble(input);
+        firstInput = false;
+        Console.WriteLine("Введите операцию");
+    }
+    else
+    {
+        if (input.Length > 0 && TwoNumOperators.Contains(input[0]))
+        {
+            char operation = input[0];
+            Console.WriteLine("Введите второе число");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    val += secondNumber;
+                    break;
+                case '-':
+                    val -= secondNumber;
+                    break;
+                case '*':
+                    val *= secondNumber;
+                    break;
+                case '/':
+                    if (secondNumber != 0)
+                        val /= secondNumber;
+                    else
+                        Console.WriteLine("Ошибка: деление на ноль!");
+                    break;
+                case '%':
+                    val %= secondNumber;
+                    break;
+                case '^':
+                    val = Math.Pow(val, secondNumber);
+                    break;
+            }
+            Console.WriteLine("Результат: " + val);
+            firstInput = true;
+        }
+        else if (OneNumOperators.Contains(input))
+        {
+            switch (input)
+            {
+                case "M+":
+                    memory += val;
+                    break;
+                case "M-":
+                    memory -= val;
+                    break;
+                case "MR":
+                    Console.WriteLine("Память: " + memory);
+                    break;
+            }
+            firstInput = true;
+        }
+        else
+        {
+            Console.WriteLine("Введена не поддерживаемая операция, введите другую");
         }
     }
 }
